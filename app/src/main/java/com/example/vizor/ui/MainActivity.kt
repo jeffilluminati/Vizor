9081cecf8baa54b7338fbaa1e983953baac928b3
DaveTankx <davetankx@gmail.com>
2021-02-25 07:42:25 +0800

    Navigation Starting implementation, added info fragment, fragment_info.xml

--- app/src/main/java/com/example/vizor/ui/MainActivity.kt
+++ app/src/main/java/com/example/vizor/ui/MainActivity.kt
@@ -1,12 +1,19 @@
-package com.example.vizor
+package com.example.vizor.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.example.vizor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("hello world")
+
+        var navControl = Navigation.findNavController(this, R.id.navHostFragment)
+//        findViewById<Button>(R.id.infoBtn).setOnClickListener(){
+//        }
     }
 }
