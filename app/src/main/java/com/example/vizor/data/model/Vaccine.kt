8872a0cc9cc5f1cb18af31ec7ab67897a96acb9f
Mayukh Das <mayukh4321@gmail.com>
2021-03-04 12:10:38 +0800

    modified User and addded Vaccine and removed Event

---
+++ app/src/main/java/com/example/vizor/data/model/Vaccine.kt
@@ -0,0 +1,4 @@
+package com.example.vizor.data.model
+
+data class Vaccine(var diseaseName: String, var vaccineStatus: VaccineStatus, ) {
+}
---
+++ app/src/main/java/com/example/vizor/data/model/VaccineStatus.kt
@@ -0,0 +1,9 @@
+package com.example.vizor.data.model
+
+import java.util.*
+
+enum class VaccineStatus() {
+    NO_VACCINE,
+    PENDING,
+    RECEIVED
+}
