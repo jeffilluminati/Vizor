911bf62fc4c6899b01872c87383b0a8fabd72ab6
DaveTankx <davetankx@gmail.com>
2021-03-05 10:04:48 +0800

    created profile page

--- app/src/main/java/com/example/vizor/ui/IdFragment.kt
+++ app/src/main/java/com/example/vizor/ui/IdFragment.kt
@@ -10,7 +10,8 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.vizor.R
 
-class IdFragment : Fragment(){
+class IdFragment : Fragment(), View.OnClickListener {
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +21,16 @@ class IdFragment : Fragment(){
 
         var a = inflater.inflate(R.layout.fragment_id, container, false)
         return a
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        requireView().findViewById<Button>(R.id.settingsBtn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.settingsBtn -> navController.navigate(R.id.action_navigation_id_to_profileFragment)
+        }
     }
 }
