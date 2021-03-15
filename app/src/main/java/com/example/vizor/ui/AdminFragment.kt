f29b26e8d596e63096b561236d2945ebd3a96458
DaveTankx <davetankx@gmail.com>
2021-03-15 19:50:49 +0800

    bug fixes in admin

--- app/src/main/java/com/example/vizor/ui/AdminFragment.kt
+++ app/src/main/java/com/example/vizor/ui/AdminFragment.kt
@@ -2,11 +2,16 @@ package com.example.vizor.ui
 
 import android.opengl.Visibility
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.vizor.R
 import com.g00fy2.quickie.QRResult
 import com.g00fy2.quickie.ScanQRCode
@@ -29,16 +34,62 @@ class AdminFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_admin2, container, false)
+        val root = inflater.inflate(R.layout.fragment_admin, container, false)
 
-        requireView().findViewById<EditText>(R.id.adminDiseaseSearch).visibility = View.INVISIBLE
+//        requireView().findViewById<EditText>(R.id.adminDiseaseSearch).visibility = View.INVISIBLE
 
         return root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        scanQrCode.launch(null)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var recyclerView = this.requireView().findViewById<RecyclerView>(R.id.adminDiseaseRecyclerView)
+        recyclerView?.layoutManager = GridLayoutManager(requireContext(),2)
+        recyclerView?.adapter = AdminDiseaseRecyclerViewAdapter()
+
+        val searchTo = requireView().findViewById<EditText>(R.id.adminDiseaseSearch)
+
+        searchTo.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+
+                // you can call or do what you want with your EditText here
+
+                var posList = listOf<Int>()
+                var diseaseList = (recyclerView!!.adapter as AdminDiseaseRecyclerViewAdapter).getDiseaseList()
+                for (a in diseaseList.indices){
+                    Log.i(diseaseList[a].toLowerCase(), requireView().findViewById<EditText>(R.id.adminDiseaseSearch).text.toString().toLowerCase())
+                    if (diseaseList[a].toLowerCase().contains(requireView().findViewById<EditText>(R.id.adminDiseaseSearch).text.toString().toLowerCase())){
+                        posList += a
+                    }
+                }
+
+
+                (recyclerView!!.adapter as AdminDiseaseRecyclerViewAdapter).updateArrays(posList.toTypedArray())
+                // yourEditText...
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                Log.i(s.toString(), "A")
+                // you can call or do what you want with your EditText here
+
+                var posList = listOf<Int>()
+                var diseaseList = (recyclerView!!.adapter as AdminDiseaseRecyclerViewAdapter).getDiseaseList()
+                for (a in diseaseList.indices){
+                    Log.i(diseaseList[a].toLowerCase(), requireView().findViewById<EditText>(R.id.adminDiseaseSearch).text.toString().toLowerCase())
+                    if (diseaseList[a].toLowerCase().contains(requireView().findViewById<EditText>(R.id.adminDiseaseSearch).text.toString().toLowerCase())){
+                        posList += a
+                    }
+                }
+
+
+                (recyclerView!!.adapter as AdminDiseaseRecyclerViewAdapter).updateArrays(posList.toTypedArray())
+            }
+        })
+
+        val vaccinationStatus = arrayOf("Vaccine Pending", "Vaccine Received", "No Vaccine", "Vaccine Pending", "Vaccine Received", "Vaccine Pending", "Vaccine Received", "No Vaccine")
+        (recyclerView!!.adapter as AdminDiseaseRecyclerViewAdapter).setStatuses(vaccinationStatus)
     }
 }
